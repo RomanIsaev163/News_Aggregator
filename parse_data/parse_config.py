5e1fecaf7039bd1a0e003b03097d9b8f29572544
RomanIsaev <mr.romka.01@mail.ru>
2023-05-12 15:10:45 +0300

    add parsing configs

---
+++ parse_data/parse_config.py
@@ -0,0 +1,11 @@
+API_ID = 27950605
+API_HASH = '4ec1f36ae431d29e7ecd57ba2853d9c7'
+#phone_number = #YOUR PHONE_NUBER
+#token = #YOUR TOKEN
+channel_list = ['startupoftheday','mytar_rf', 'businesstodayy',
+                'exploitex', 'ostorozhno_novosti', 'techno_news_tg',
+                'd_code', 'FatCat18', 'bolecon', 'AK47pfl']
+#dir_to_save = "./data/processed"
+date_format = '%Y-%m-%d'
+limit = 1000
+parse_from_date = '2022-01-01'
